36929c3e8af6efe1e0a06653ffa0aef3fa53cb2f
jin <jintoknetplayer@gmail.com>
2020-11-23 12:28:32 +0800

    follow linter advise;

--- src/main/java/com/play/jacoco/App.java
+++ src/main/java/com/play/jacoco/App.java
@@ -2,6 +2,10 @@ package com.play.jacoco;
 
 public class App {
 
+    private App() {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hi!");
     }
---
+++ src/main/java/com/play/jacoco/package-info.java
@@ -0,0 +1,4 @@
+package com.play.jacoco;
+/*
+Simple app
+ */
